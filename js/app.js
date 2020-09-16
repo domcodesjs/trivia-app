83696d9dad653b52de8369ffb0885b03c20574ef
Dominic Faulring <domcodesjs@gmail.com>
2020-09-16 09:23:12 -0700

    can start and proceed through an array of questions and output a end game scrn

--- js/app.js
+++ js/app.js
@@ -9,7 +9,12 @@ const store = {
     {
       question: 'What is the current year?',
       answers: ['1970', '2020', '2019', '2005'],
-      correctAnswer: '2019'
+      correctAnswer: '2020'
+    },
+    {
+      question: 'What is the day?',
+      answers: ['Monday', 'Tuesday', 'Thursday', 'Friday'],
+      correctAnswer: 'Friday'
     }
   ],
   quizStarted: false,
@@ -19,14 +24,19 @@ const store = {
 
 /********** TEMPLATE GENERATION FUNCTIONS **********/
 function createQuestion(question) {
-  console.log(question.answers);
   return `
-    <div>
+    <div class="trivia-question">
       <h1>${question.question}</h1>
-      <ul>
+      <ul class="trivia-question-answers">
         ${question.answers.map((answer) => `<li>${answer}</li>`).join('')}
       </ul>
     </div>
+  `;
+}
+
+function createEndGameScreen() {
+  return `
+    You got ${store.score} out of ${store.questions.length} right!
   `;
 }
 
@@ -34,19 +44,24 @@ function createStartButton() {
   return '<button>Start</button>';
 }
 
-function createSubmitButton() {
-  return '<button>Submit Answer</button>';
-}
+// optional
+// function createSubmitButton() {
+//   return '<button>Submit Answer</button>';
+// }
 
 function init() {
   $('main').append(createStartButton());
-  $('button').on('click', () => renderQuestion(store));
+  $('button').on('click', startBtnClick);
 }
 
 /********** RENDER FUNCTION(S) **********/
-function renderQuestion(store) {
-  $('main').append(createQuestion(store.questions[0]));
-  $('button').remove();
+function renderQuestion(question) {
+  $('main').html(createQuestion(question));
+  $('.trivia-question-answers').on('click', 'li', answerOnClick);
+}
+
+function renderEndGame() {
+  return $('main').html(createEndGameScreen());
 }
 
 /********** EVENT HANDLER FUNCTIONS **********/
@@ -54,7 +69,32 @@ function checkAnswer(question, givenAnswer) {
   return question.correctAnswer === givenAnswer;
 }
 
-function answerOnClick() {}
+function startBtnClick() {
+  store.quizStarted = true;
+  renderQuestion(store.questions[0]);
+}
+
+function answerOnClick() {
+  if (checkAnswer(store.questions[store.questionNumber], $(this).text())) {
+    store.score += 1;
+    store.questionNumber += 1;
+    return isGameDone()
+      ? renderEndGame()
+      : renderQuestion(store.questions[store.questionNumber]);
+  } else {
+    store.questionNumber += 1;
+    return isGameDone()
+      ? renderEndGame()
+      : renderQuestion(store.questions[store.questionNumber]);
+  }
+}
+
+function isGameDone() {
+  if (store.questionNumber <= store.questions.length - 1) {
+    return false;
+  }
+  return true;
+}
 
 // These functions handle events (submit, click, etc)
 $(init);
